9704b45ed9de529e2b309ece471e8af1034095f5
H-ymt <y.handai1272@gmail.com>
2023-06-29 00:12:36 +0900

    fix border-radiusの調整

--- 02/app.js
+++ 02/app.js
@@ -3,13 +3,14 @@ const tl = gsap.timeline();
 
 tl.to(bg, {
   borderRadius: "0",
+  bottom: "0",
   height: "100vh",
   width: "100vw",
   ease: "none",
   scrollTrigger: {
-    trigger: bg,
+    trigger: "body",
     start: "top center",
-    end: "bottom center",
+    end: "bottom+=300px center",
     scrub: true,
   },
 });
